1bee787bcf9c0dd48ffe4b85596cb182299e8833
Danila Molodtsov <danmol04@gmail.com>
2020-07-24 09:09:21 +0300

    gh pages test 2

---
+++ build/precache-manifest.3cc6802b5b1d626fb15e033ae828974c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c165e656d0276613ff327f8718e69f7",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "0a49f38d76f26ca5fb0a",
+    "url": "/social-network/static/css/main.5605a966.chunk.css"
+  },
+  {
+    "revision": "a60bb109df4206445791",
+    "url": "/social-network/static/js/2.67e06bb7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/social-network/static/js/2.67e06bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a49f38d76f26ca5fb0a",
+    "url": "/social-network/static/js/main.5a44319e.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  },
+  {
+    "revision": "56397e007532c05f39537f2a5b538cf1",
+    "url": "/social-network/static/media/delete.56397e00.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-network/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3c8e2cbbbb09387c7a76740b5790ea73",
+    "url": "/social-network/static/media/message.3c8e2cbb.svg"
+  },
+  {
+    "revision": "19142cc51b8c074217df3d7499267bf0",
+    "url": "/social-network/static/media/search.19142cc5.svg"
+  }
+]);
